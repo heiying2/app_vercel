49bf8d7dc567bdea8a0f970f87ab1e6173593dc8
Hsu, Chih-Chao <chihchao@go.edu.tw>
2025-08-17 23:12:23 +0800

    fastapi

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Test test Edit <code>src/App.jsx</code> and save to test HMR
+          Go to Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
