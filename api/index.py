97ceb14b8d689ba70f9054b8625dea92ace2d785
Hsu, Chih-Chao <chihchao@go.edu.tw>
2025-08-18 17:02:14 +0800

    app

--- api/index.py
+++ api/index.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 
-# 創建一個 FastAPI 應用實例
 app = FastAPI()
 
-# 定義一個路由，當用戶訪問根路徑時，返回一個 JSON 響應
-@app.get("/hello")
+@app.get("/app/hello")
 def read_root():
     return {"Hello": "World"}
 
