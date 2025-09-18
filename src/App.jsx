ed9982a3199ecf35c5e359c4269aff37b9db0810
heiying2 <heiforeal2@gmail.com>
2025-09-18 19:36:15 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,3 +22,24 @@ export default function App() {
     </div>
   );
 }
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Marketing from "./pages/Marketing";
+import Finance from "./pages/Finance";
+import Procurement from "./pages/Procurement";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/marketing" element={<Marketing />} />
+        <Route path="/finance" element={<Finance />} />
+        <Route path="/procurement" element={<Procurement />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
