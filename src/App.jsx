0b5e29094f6acf8fc5f73b8c7ee63d367e2eebfb
heiying2 <heiforeal2@gmail.com>
2025-09-18 19:38:55 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,4 @@
-import { useState } from "react";
-import Marketing from "./pages/Marketing";
-import Finance from "./pages/Finance";
-import Procurement from "./pages/Procurement";
 
-export default function App() {
-  const [page, setPage] = useState("marketing");
-
-  return (
-    <div>
-      <nav>
-        <button onClick={() => setPage("marketing")}>行銷</button>
-        <button onClick={() => setPage("finance")}>財務</button>
-        <button onClick={() => setPage("procurement")}>採購</button>
-      </nav>
-
-      <div>
-        {page === "marketing" && <Marketing />}
-        {page === "finance" && <Finance />}
-        {page === "procurement" && <Procurement />}
-      </div>
-    </div>
-  );
-}
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
