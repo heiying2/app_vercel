83aaababf40c51013aa425ab2e1f5e8e25cff85f
heiying2 <heiforeal2@gmail.com>
2025-09-18 19:12:47 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,15 @@
+export async function getMarketingAdvice(keyword) {
+  const res = await fetch(`/app/marketing?keyword=${keyword}`);
+  return await res.json();
+}
+
+export async function getFinanceAdvice(cost, price) {
+  const res = await fetch(`/app/finance?cost=${cost}&price=${price}`);
+  return await res.json();
+}
+
+export async function getProcurementAdvice(stock, forecast) {
+  const res = await fetch(`/app/procurement?stock=${stock}&forecast=${forecast}`);
+  return await res.json();
+}
 
